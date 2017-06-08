6e705d351c64f698c47e4b88eafc8d725ff6eac4
Caroline Wood <carolinemarieweb@gmail.com>
2017-06-08 12:04:31 +0100

    updated functions.php and style.css

--- functions.php
+++ functions.php
@@ -10,3 +10,14 @@
  */
 remove_filter( 'the_content', 'wpautop' );
 remove_filter( 'the_excerpt', 'wpautop' );
+
+/**
+ * TN - Remove Query String from Static Resources. See http://bit.ly/2rYGwBo
+ */
+function remove_css_js_ver( $src ) {
+if( strpos( $src, '?ver=' ) )
+$src = remove_query_arg( 'ver', $src );
+return $src;
+}
+add_filter( 'style_loader_src', 'remove_css_js_ver', 10, 2 );
+add_filter( 'script_loader_src', 'remove_css_js_ver', 10, 2 );
